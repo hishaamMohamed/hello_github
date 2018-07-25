3e3e1f75279dc5e939b2cfc4934e083b33c3105b
Hishaam <mhisha361@apac.comcast.com>
2018-07-25 14:49:44 +0530

    included python file

---
+++ dd.py
@@ -0,0 +1,48 @@
+
+
+##a=float(input("age")) # age is going to a
+##print(a)
+
+
+
+##
+##age,name=21,"hishaam"
+##print("the age is {1} and name is {2} :",age,name)
+
+print("hello jenkins")
+
+
+##a,b=2,3
+##
+##c=a+b
+##print(c)
+##d=a**3
+##print(d)
+##d=a//2
+
+
+
+##a=int(input("enter the number"))
+##if a==2:
+##    print("number is 2")
+##
+##
+##    print("it is 2")
+##else:
+##    print("number is not 2")
+
+
+##while True:
+##    a=int(input("enter the number"))
+##    if a==2:
+##        print("number is 2")
+##
+##
+##        print("it is 2")
+##    elif a==0:
+##        break
+##        
+##            
+##    else:
+##        print("number is not 2")
+##    
